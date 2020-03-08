e02b83d1775f0af8e5e9097fee5cbb5a75e34ffe
dan <dan.taylor@observian.com>
2020-03-07 22:21:46 -0600

    miskeyed variable

--- cloud-run.tf
+++ cloud-run.tf
@@ -5,7 +5,7 @@ resource "google_cloud_run_service" "statusupdater" {
   template {
     spec {
       containers {
-        image = "gcr.io/${var.project}/statusupdater"
+        image = "gcr.io/${var.project-name}/statusupdater"
         env {
           name = "SOURCE"
           value = "remote"
