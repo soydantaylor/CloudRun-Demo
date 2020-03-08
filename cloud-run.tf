5d8b7e329d2868c8dfb0f2784e9d976f55c3b416
dan <dan.taylor@observian.com>
2020-03-07 22:55:58 -0600

    wrong syntax for replacement value

--- cloud-run.tf
+++ cloud-run.tf
@@ -5,7 +5,7 @@ resource "google_cloud_run_service" "statusupdater" {
   template {
     spec {
       containers {
-        image = "<<IMAGE>>"
+        image = "jjIMAGEjj"
         env {
           name = "SOURCE"
           value = "remote"
