c1761063b19f3c264007b4b9515759845704e6f1
dan <dan.taylor@observian.com>
2020-04-09 05:31:30 -0600

    bumping version

--- StatusUpdater/Controllers/WeatherForecastController.cs
+++ StatusUpdater/Controllers/WeatherForecastController.cs
@@ -24,16 +24,9 @@ namespace StatusUpdater.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IActionResult Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Ok("Minor version 0.1");
         }
     }
 }
