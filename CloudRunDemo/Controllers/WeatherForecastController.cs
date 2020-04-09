097b69eed03acc5fe65a9f6a53308ebe46847134
dan <dan.taylor@observian.com>
2020-04-09 11:49:33 -0600

    adding foo

--- CloudRunDemo/Controllers/WeatherForecastController.cs
+++ CloudRunDemo/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 namespace CloudRunDemo.Controllers
 {
     [ApiController]
-    [Route("/")]
+    [Route("/foo")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
