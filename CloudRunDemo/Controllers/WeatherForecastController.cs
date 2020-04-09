c5c749ea890955be4eb8e90377fcb132e5afe013
dan <dan.taylor@observian.com>
2020-04-09 07:34:57 -0600

    respond on root

--- CloudRunDemo/Controllers/WeatherForecastController.cs
+++ CloudRunDemo/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 namespace CloudRunDemo.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("/")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
